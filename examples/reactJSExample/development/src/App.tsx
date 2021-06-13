8dfb1daf898479b1512095dafbce953ce8f87daa
vzakharchenko <vzakharchenko@modeln.com>
2021-06-13 23:44:28 +0300

    fixed build

--- examples/reactJSExample/development/src/App.tsx
+++ examples/reactJSExample/development/src/App.tsx
@@ -1,51 +1,55 @@
-import * as React from 'react';
 import {decode} from "jsonwebtoken";
-import {fetchData} from "./restCalls";
 import {Card, Spin, Typography} from "antd";
 import Title from "antd/es/typography/Title";
 import Paragraph from "antd/es/typography/Paragraph";
+// eslint-disable-next-line  no-use-before-define
+import * as React from 'react';
+
+import {fetchData} from "./restCalls";
+
 
 export default class App extends React.Component {
-    state = {
+  state = {
+    loading: true,
+    token: {
+      email: "",
+      name: "",
+      // eslint-disable-next-line babel/camelcase
+      preferred_username: "",
+    },
+    jwt: '',
+  };
+
+  async componentDidMount() {
+    try {
+      this.setState({
         loading: true,
-        token: {
-            email: "",
-            name: "",
-            preferred_username: "",
-        },
-        jwt: ''
-    };
-
-    async componentDidMount() {
-        try {
-            this.setState({
-                loading: true,
-            });
-            const tokenString = await fetchData('/token', 'GET', {});
-            const accessToken = JSON.parse(tokenString).activeToken;
-            this.state.jwt = accessToken;
-            this.setState({
-                token: decode(accessToken),
-                jwt: accessToken,
-                loading: false,
-            });
-        } catch (e) {
-            this.setState({
-                loading: false,
-                jwt: e.message
-            });
-        }
-
+      });
+      const tokenString = await fetchData('/token', 'GET', {});
+      const accessToken = JSON.parse(tokenString).activeToken;
+      this.state.jwt = accessToken;
+      this.setState({
+        token: decode(accessToken),
+        jwt: accessToken,
+        loading: false,
+      });
+    } catch (e) {
+      this.setState({
+        loading: false,
+        jwt: e.message,
+      });
     }
 
-    render() {
+  }
 
-        return this.state.loading ? (<Spin tip="Get Access Token...">
+  render() {
+
+    return this.state.loading ? (<Spin tip="Get Access Token...">
         </Spin>) : (
             <div>
                 <Typography>
                     <Title>Current User Info</Title>
-                    <Card title={this.state.token.preferred_username} style={{ width: 300 }}>
+                    <Card title={this.state.token.preferred_username} style={{width: 300}}>
                         <p>{this.state.token.name}</p>
                         <p>{this.state.token.email}</p>
                     </Card>
@@ -60,5 +64,5 @@ export default class App extends React.Component {
                 </Typography>
             </div>
         );
-    }
+  }
 }
--- examples/reactJSExample/development/src/index.tsx
+++ examples/reactJSExample/development/src/index.tsx
@@ -1,11 +1,10 @@
-import * as React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 import 'antd/dist/antd.css';
-import {DatePicker} from "antd";
-import App from "./App";
 
+import App from "./App";
 
 
 render(
     <App/>,
-    document.getElementById('root'));
+    document.getElementById('root'),
+);
