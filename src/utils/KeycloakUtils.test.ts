8cb4ecb87ba516b811986a537c6af05da178f42b
vzakharchenko <vzakharchenko@modeln.com>
2021-07-07 16:56:11 +0300

    added Unit Tests

--- src/utils/KeycloakUtils.test.ts
+++ src/utils/KeycloakUtils.test.ts
@@ -6,9 +6,18 @@ import 'jest';
 import {RequestObject, ResponseObject} from "../index";
 import {APIGateWayOptions} from "../apigateway/ApiGateway";
 import {StrorageDB, StrorageDBType} from "../session/storage/Strorage";
+import {PublicUrlPageHandler} from "../handlers/PublicUrlPageHandler";
+import {SingleTenantUrlPageHandler} from "../handlers/SingleTenantUrlPageHandler";
 
 import {initOptions} from "./DefaultPageHandlers";
-import {getCurrentHost, getCurrentStorage, getHostName, getSessionName} from "./KeycloakUtils";
+import {
+  getCurrentHost,
+  getCurrentStorage,
+  getCustomPageHandler,
+  getHostName,
+  getKeycloakJsonFunction,
+  getSessionName, isCustomBehavior,
+} from "./KeycloakUtils";
 
 const request: RequestObject = {
   baseUrl: '/',
@@ -68,6 +77,7 @@ describe('KeycloakUtils tests', () => {
     // @ts-ignore
     handlerOptions = initOptions(defOptions);
     handlerOptions.session.sessionConfiguration.sessionCookieName = 'TEST';
+    handlerOptions.pageHandlers = [];
   });
 
   test('test PublicUrlPageHandler getHostName', async () => {
@@ -119,6 +129,43 @@ describe('KeycloakUtils tests', () => {
   test('test PublicUrlPageHandler getCurrentStorage CustomDB', async () => {
     handlerOptions.session.sessionConfiguration.storageType = new DummyStorageDB();
     await getCurrentStorage(handlerOptions);
+  });
+
+  test('test PublicUrlPageHandler getCurrentStorage getKeycloakJsonFunction', async () => {
+    expect(await getKeycloakJsonFunction({})).toEqual({});
+  });
+
+  test('test PublicUrlPageHandler getCurrentStorage isCustomBehavior 1', async () => {
+    expect(await isCustomBehavior('public', {request, options: handlerOptions})).toEqual(null);
+  });
+
+
+  test('test PublicUrlPageHandler getCurrentStorage isCustomBehavior 2', async () => {
+    const publicUrlPageHandler = new PublicUrlPageHandler('/');
+    handlerOptions.pageHandlers?.push(publicUrlPageHandler);
+    expect(await isCustomBehavior('public', {request, options: handlerOptions})).toEqual('public');
+  });
+
+  test('test PublicUrlPageHandler getCurrentStorage isCustomBehavior 3', async () => {
+    const singleTenantUrlPageHandler = new SingleTenantUrlPageHandler('/');
+    handlerOptions.pageHandlers?.push(singleTenantUrlPageHandler);
+    expect(await isCustomBehavior('public', {request, options: handlerOptions})).toEqual(null);
+  });
+
+  test('test PublicUrlPageHandler getCurrentStorage getCustomPageHandler 1', async () => {
+    expect(await getCustomPageHandler('public', request, handlerOptions)).toEqual(null);
+  });
+
+  test('test PublicUrlPageHandler getCurrentStorage getCustomPageHandler 3', async () => {
+    const singleTenantUrlPageHandler = new SingleTenantUrlPageHandler('/');
+    handlerOptions.pageHandlers?.push(singleTenantUrlPageHandler);
+    expect(await getCustomPageHandler('public', request, handlerOptions)).toEqual(null);
+  });
+
+  test('test PublicUrlPageHandler getCurrentStorage getCustomPageHandler 2', async () => {
+    const publicUrlPageHandler = new PublicUrlPageHandler('/');
+    handlerOptions.pageHandlers?.push(publicUrlPageHandler);
+    expect(await getCustomPageHandler('public', request, handlerOptions)).toEqual(publicUrlPageHandler);
   });
 
   test('test PublicUrlPageHandler getCurrentStorage Error', async () => {
